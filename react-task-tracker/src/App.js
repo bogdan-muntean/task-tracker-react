9b749566187f47e7fc0c3ca6569665b55e9e9bfa
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-09-02 14:33:05 +0300

    transfer tasks array in App.js

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Header from './Header';
-import Tasks from './Tasks'
+import Tasks from './Tasks';
+import {useState} from 'react';
+
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Meeting',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting2',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Meeting3',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+  ])
+
   return (
     <div className="container">
       <Header/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- react-task-tracker/src/Tasks.js
+++ react-task-tracker/src/Tasks.js
@@ -1,32 +1,10 @@
 import React from 'react'
-import {useState} from 'react'
 // import ReactDOM from 'react-dom/client';
 // import PropTypes from 'prop-types'
 
 
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-            {
-                id: 1,
-                text: 'Meeting',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 2,
-                text: 'Meeting2',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 3,
-                text: 'Meeting3',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-        ])
-
+const Tasks = ({tasks}) => {
     return (
     <>
         {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
