20139e43e8189b6aead35c23a793a6bc69bcadef
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-09-07 18:52:24 +0300

    add hooks for Form inputs

--- react-task-tracker/src/AddTask.js
+++ react-task-tracker/src/AddTask.js
@@ -1,22 +1,35 @@
+import { useState } from 'react'
 
 const AddTask = () => {
-  return (
-    <form className="add-form">
-        <div className="form-input">
-            <label>Task</label>
-            <input type="text" placeholder="Add task"></input>
-        </div>
-        <div className="form-input">
-            <label>Day and Time</label>
-            <input type="text" placeholder="Add day and time"></input>
-        </div>
-        <div className="form-input">
-            <label>Set Reminder</label>
-            <input type="checkbox"></input>
-        </div>
-        <input type="submit" value="Save Task"></input>
-    </form>
-  )
+    const [text, setText] = useState('')
+    const [time, setTime] = useState('')
+    const [reminder, setReminder] = useState(false)
+    return (
+        <form className="add-form">
+            <div className="form-input">
+                <label>Task</label>
+                <input type="text" 
+                placeholder="Add task" 
+                value={text} 
+                onChange={(e) => setText(e.target.value)}></input>
+            </div>
+            <div className="form-input">
+                <label>Day and Time</label>
+                <input type="text" 
+                placeholder="Add day and time"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}></input>
+            </div>
+            <div className="form-input">
+                <label>Set Reminder</label>
+                <input type="checkbox"
+                checked={reminder}
+                value={reminder}
+                onChange={(e) => setReminder(e.currentTarget.checked)}></input>
+            </div>
+            <input type="submit" value="Save Task"></input>
+        </form>
+    )
 }
 
 export default AddTask
